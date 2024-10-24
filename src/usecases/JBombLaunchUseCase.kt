f358e29f9ca0252c875f004f2b94552a9d7fadb2
Simone Di Berardino <simonediberardino7@gmail.com>
2024-10-24 20:19:00 +0200

    Disabled ui scaling

--- src/usecases/JBombLaunchUseCase.kt
+++ src/usecases/JBombLaunchUseCase.kt
@@ -5,11 +5,17 @@ import java.io.File
 import java.io.IOException
 import kotlin.system.exitProcess
 
-class JBombLaunchUseCase: UseCase<Unit> {
+class JBombLaunchUseCase : UseCase<Unit> {
     override suspend fun invoke() {
         val directory = "${Paths.installDirectory}/${Paths.jarPath}"
-        // Create a ProcessBuilder to run the JAR file
-        val processBuilder = ProcessBuilder("java", "-jar", directory)
+        // Create a ProcessBuilder to run the JAR file with both DPI and UI scaling disabled
+        val processBuilder = ProcessBuilder(
+            "java",
+            "-Dsun.java2d.dpiaware=false", // Disables Java DPI awareness
+            "-Dsun.java2d.uiScale=1",      // Disables UI scaling by setting it to 1 (no scaling)
+            "-jar",
+            directory
+        )
 
         try {
             // Start the process
